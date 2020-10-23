a9428e184f6b55035dc8fa2cc25a3a893168cc71
Doughnut <56208915+The-Doughnut@users.noreply.github.com>
2020-10-23 01:18:38 -0400

    Add files via upload

--- doughnut.js
+++ doughnut.js
@@ -1,1 +1,22 @@
-//lol there is no javascript
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  var ms = today.getMilliseconds();
+  m = addZero(m);
+  s = addZero(s);
+  ms = addZeroms(ms);
+  document.getElementById('time').innerHTML = "Current time: " + h + ":" + m + ":" + s + ":" + ms + "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Today's weather for Montr&eacute;al: 23&deg;C with 40% chance of showers for October 23, 2020";
+  var t = setTimeout(startTime, 1);
+}
+function addZero(i) {
+  if (i < 10) {i = "0" + i};
+  return i;
+}
+
+function addZeroms(i){
+	if (i < 10) {i = "00" + i};
+	if (i < 100) {i = "0" + i};
+  return i;
+}
