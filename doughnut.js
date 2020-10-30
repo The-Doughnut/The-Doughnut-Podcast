6681ee7005fc81d97612e5b2bd454dfbaa21cd3f
Doughnut <56208915+The-Doughnut@users.noreply.github.com>
2020-10-30 01:13:26 -0400

    Update doughnut.js

--- doughnut.js
+++ doughnut.js
@@ -7,7 +7,7 @@ function startTime() {
   m = addZero(m);
   s = addZero(s);
   ms = addZeroms(ms);
-  document.getElementById('time').innerHTML = "Current time: " + h + ":" + m + ":" + s + ":" + ms + "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Today's weather for Montr&eacute;al: high 3&deg;C, low -5&deg;C. Mainly sunny October 30, 2020 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp UV Index: 2";
+  document.getElementById('time').innerHTML = "Current time: " + h + ":" + m + ":" + s + ":" + ms + "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Today's weather for Montr&eacute;al: high 3&deg;C, low -5&deg;C. Mainly sunny for October 30, 2020 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp UV Index: 2";
   var t = setTimeout(startTime, 1);
 }
 function addZero(i) {
